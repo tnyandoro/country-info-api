0f05541d0bbc5b6609fd3cef212f6884bca59340
Tendai Nyandoro <tnyandoro@gmail.com>
2024-06-23 16:18:42 +0200

    Add: countries controller, and set a default value for the country status

--- app/controllers/v1/countries_controller.rb
+++ app/controllers/v1/countries_controller.rb
@@ -1,18 +1,47 @@
 module V1
   class CountriesController < ApplicationController
-    def create; end
+    def create
+      puts(">>>>>>>> HERE <<<<<<<<<<<<<")
+      country = Country.new(country_params)
 
-    def index; end
-    
+      if country.save
+        json_response(country, :created)
+      else
+        json_response(country.errors, :unprocessable_entity)
+      end
+    end
+
+    def index
+      countries = Country.where(status: 'active')
+      json_response(countries, :ok)
+    end
+
     def show
       country_code = params[:code]
       @country = Country.find_by(alpha_2_code: country_code) || Country.find_by(alpha_3_code: country_code)
 
       if @country
-        json_response(@country, status: 200)
+        json_response(@country)
       else
-        json_response({ error: 'Country not found' }, status: 404)
+        json_response({ error: 'Country not found' }, :not_found)
       end
+    end
+
+    def delete
+      country = Country.find(params[:id])
+
+      if country
+        country.update(status: 'archived')
+        json_response(country.reload, :ok)
+      else
+        json_response({ error: 'No active country with that ID was found' }, :not_found)
+      end
+    end
+
+    private
+
+    def country_params
+      params.require(:country).permit(:name, :currency, :alpha_2_code, :alpha_3_code, :status)
     end
   end
 end
--- app/models/country.rb
+++ app/models/country.rb
@@ -1,7 +1,13 @@
 class Country < ApplicationRecord
+  before_create :set_default_status
+
   validates :name, :currency, :alpha_2_code, :alpha_3_code, presence: true
   validates :alpha_2_code, length: { is: 2 }
   validates :alpha_3_code, length: { is: 3 }
 
-  enum :status, %(active archived)
+  private
+
+  def set_default_status
+    self.status ||= 'active'
+  end
 end
